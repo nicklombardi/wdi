cc3f808e7d539e3ae8a9aa36d21828a23a9e22d9
nicklombardi <nicklombardi@gmail.com>
2013-06-19 13:42:57 -0400

    added madlibs.rb

---
+++ assignments/madlibs.rb
@@ -0,0 +1,26 @@
+
+puts "hero please:"
+name_one = gets.chomp
+
+puts "adjective please:"
+adjective_one = gets.chomp
+
+puts "villain please:"
+noun_one = gets.chomp
+
+puts "name please:"
+name_two = gets.chomp
+
+puts "adjective please:"
+adjective_one = gets.chomp
+
+puts "adjective please:"
+adjective_two = gets.chomp
+
+
+
+
+puts "the #{adjective_one} super hero #{name_one} descended upon #{noun_one}
+only to be met by the villain #{name_two}. The confrontation was
+a #{adjective_one} struggle. The damage sustained by #{name_two} was
+#{adjective_two} while #{name_one} remained relatively unscathed."
