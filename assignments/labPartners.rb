54758f327f070f7b0154fbff2f40a63a9efa12ad
nicklombardi <nicklombardi@gmail.com>
2013-06-19 17:33:43 -0400

    added shuffle feature

--- assignments/labPartners.rb
+++ assignments/labPartners.rb
@@ -1,10 +1,10 @@
-wdi_class = ["Beryl Schragger", "Chris Clearfield", "Christine Coulter", "Christopher Huyett", "Christopher Wright", "Corbin Sykes", "Eleyna Whittingham", "Eric Gong", "Jamal Powell", "Jason Darcy", "Kevon Cheung", "Mike Mayer", "Mike Vanger", "Nick Lombardi", "Peter Lai", "Phillip Lamplugh", "Teddy Cleveland", "Thomas Yu", "Tim Blonski", "Tom Brennan", "Tricia Dougals", "Wesley Carr", "Zachariah Reitano"]
+wdi_class = ["Beryl Schragger", "Chris Clearfield", "Christine Coulter", "Christopher Huyett", "Christopher Wright", "Corbin Sykes", "Eleyna Whittingham", "Eric Gong", "Jamal Powell", "Jason Darcy", "Kevon Cheung", "Mike Mayer", "Mike Vanger", "Nick Lombardi", "Peter Lai", "Teddy Cleveland", "Thomas Yu", "Tim Blonski", "Tom Brennan", "Tricia Dougals", "Wesley Carr", "Zachariah Reitano"]
 
 def group_size
-  group_size = 5
+  group_size = 8
 end
 
-groups_formed = wdi_class.each_slice(group_size).to_a
+groups_formed = wdi_class.shuffle!.each_slice(group_size).to_a
 
 groups_formed.each do |group|
   if group.length < group_size
