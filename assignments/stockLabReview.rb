7e97b917c8b7bf7a99fe00a1d4b5624f38430f7c
nicklombardi <nicklombardi@gmail.com>
2013-06-25 10:42:40 -0400

    stockLabReview end of review

--- assignments/stockLabReview.rb
+++ assignments/stockLabReview.rb
@@ -1,41 +1,35 @@
 require 'yahoofinance'
 
-def get_price(ticker)
-  return YahooFinance::get_quotes(YahooFinance::StandardQuote, ticker)[ticker].lastTrade
+def get_stock_price(ticker)
+  return YahooFinance::get_quotes(YahooFinance::StandardQuote,
+      ticker)[ticker].lastTrade
+end
+
+def get_transaction_value(ticker, shares)
+    return get_stock_price(ticker) * shares
 end
 
 class Stock
   attr_reader :ticker, :number_of_shares
+  # ticker
+  # number of shares
   def initialize(ticker, number_of_shares)
     @ticker = ticker
     @number_of_shares = number_of_shares
   end
 
   def buy_shares(shares_to_buy)
-    @number_of_shares += shares_to_buy
+    # @number_of_shares = @number_of_shares + shares_to_add
+    @number_of_shares += shares_to_add
   end
 
   def selling_shares(shares_to_sell)
-    if @shares_to_sell < @number_of_shares
+    # @number_of_shares = @number_of_shares + shares_to_add
+    if shares_to_sell < @number_of_shares
       @number_of_shares -= shares_to_sell
     else
       return false
-  end
-
-  def get_transaction_value(ticker, shares)
-    return get_stock_price(@ticker) * shares
-  end
-
-  def get_current_value
-    return get_transaction_value(@tcker, @shares)
-  end
-
-  def calculate_portfolio_value
-    portfolio_value = 0
-    @stocks.each do |stock|
-      portfolio_value += stock.get_current_value
     end
-    return portfolio_value
   end
 
   def get_price
@@ -44,53 +38,53 @@ class Stock
 end
 
 class Portfolio
-  #value
+  # value
+  # portfolio_name
   def initialize
-    @stocks = {}
+    @stocks={}
   end
 
+  # Pass balance here?
   def add_stock(ticker, number_of_shares)
     if @stocks[ticker]
-      #already have stock
+      # we already have a stock that exists
     else
-      #add stock
-
+      # we need to add a stock object
     end
   end
 end
 
 class Client
-  #cash balance
+  # cash_balance
   def initialize(initial_cash_balance)
     @portfolios = {}
-    @cash_balance = initial_cash_balance
+    @cash_balance=initial_cash_balance
   end
 
   def add_portfolio(portfolio_name)
-    # see if portfolio exists?
-    # add it if not
+    # Check to see if portfolio exists
+    # add it if it doesn't
   end
 
   def add_stock_to_portfolio(portfolio_name, ticker, shares)
-    # check to makes ure portfolio exists
-    portfolio = @porfolios[portfolio_name]
-    if portfolio
-      if self.enough_money?(ticker, shares
-      portfolio.add_stock)(ticker, number_of_shares)
+    # XXX check to make sure portfolio exists, and if it does
+    portfolio = @portfolios[portfolio_name]
+    if self.enough_money?(ticker,shares)
+      portfolio.add_stock(ticker, number_of_shares
     else
-      puts "not enough money"
+      puts "Not enough money"
     end
   end
-
   def enough_money?(ticker, shares)
-    transaction_cost = get_transaction_value(ticker, shares)
+    transaction_cost = get_transaction_value(ticker,shares)
     if transaction_cost > @cash_balance
       return false
     else
       return true
+    end
   end
 end
 
 new_stock = Stock.new('AAPL', 100)
-puts "#{new_stock.ticker}, #{new_stock.number_of_shares}"
-
+puts "#{new_stock.ticker}, #{new_stock.number_of_shares} shares, $#{new_stock.get_price}"
+# XXX Lots of other code
